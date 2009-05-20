f6ec15af642d3c21574065b5235b6f8587a8b301
David Baldwin <baldwindavid@gmail.com>
2009-05-20 14:33:18 -0400

    initial commit

---
+++ calorie_king.rb
@@ -0,0 +1,80 @@
+require 'cgi'
+require 'rubygems'
+require 'httparty'
+require 'mash'
+require 'values_to_proper_class'
+
+module Kernel
+  
+  # if an object is not already an array, place it inside one
+  def ensure_a
+    self.is_a?(Array) ? self : [self]
+  end
+
+end
+
+class CalorieKing
+  
+  include HTTParty
+  basic_auth '84294d7ae4454189bf2a3ebc37a0e421', ''
+  base_uri 'http://foodsearch1.webservices.calorieking.com/rest'
+  
+  def self.urlencode(text)
+    CGI::escape(text)
+  end
+
+  # enter a search term and receive a list of possible matches within categories
+  
+  # Example:  
+  # categories = CalorieKing.search 'Big Mac'
+  
+  # >> categories.first.name
+  #   => "McDonald's"
+  
+  # >> categories.first.foods.first.name
+  #   => "Sandwiches, Big Mac"
+  
+  # >> categories.first.foods.first.id
+  #   => "6adc1bda-24f6-42a8-87ee-4de0ba87ac96"
+
+  def self.search(term)
+    begin
+      categories = get("/search/#{urlencode(term)}")['searchresults']['category'].ensure_a
+      categories = categories.collect do |c| 
+        Mash.new({ 
+          "name" => c['name'],
+          "foods" => c['foods']['food'].ensure_a.collect {|f| {"name" => f["display"], "id" => f["id"]} }
+        })
+      end
+      categories
+    rescue
+      puts "Sorry, but we couldn't find anything remotely close to that food."
+    end
+  end
+  
+  # enter a food id and receive a hash containing the food, it's available serving types (cup, gram, teaspoon) and associated nutritional data
+  # nutritional data will be returned in appropriate class (integers, floats, etc.) rather than just as a string
+  
+  # Example:  
+  # food = CalorieKing.find_by_id "6adc1bda-24f6-42a8-87ee-4de0ba87ac96"
+  
+  # >> food.name
+  #   => "McDonald's: Sandwiches & Burgers: Sandwiches, Big Mac"
+  
+  # >> food.servings.first.name
+  #   => "sandwich (7.5 oz)"
+  
+  # >> food.servings.first.nutrients.calories
+  #   => 540
+  
+  def self.find_by_id(id)
+    begin
+      food = get("/foods/#{id}")['food']
+      food["servings"] = food['servings']['serving'].ensure_a
+      Mash.new(food.values_to_proper_class)
+    rescue
+      puts "Sorry, but we couldn't find a food with that id."
+    end
+  end
+  
+end
---
+++ values_to_proper_class.rb
@@ -0,0 +1,83 @@
+module Kernel
+  def to_proper_class
+    self
+  end
+end
+
+class NilClass
+  def to_proper_class
+    nil
+  end
+end
+
+class Array
+  
+  def values_to_proper_class(symbolize_keys = true)
+    inject([]) do |memo, value|     
+      memo << 
+        if [Array, Hash].include? value.class
+          value.values_to_proper_class(symbolize_keys)
+        else
+          value.to_proper_class
+        end
+    end
+  end
+  
+end
+
+
+class Hash
+  
+  def values_to_proper_class(symbolize_keys = true)
+    inject({}) do |memo, values|
+      key, value = values
+      key = symbolize_keys ? key.to_sym : key.to_s
+      memo[key] = 
+        if [Array, Hash].include? value.class
+          value.values_to_proper_class(symbolize_keys)
+        else
+          value.to_proper_class
+        end
+      memo
+    end
+  end
+
+end
+
+class String
+  
+  def to_proper_class
+    if boolean?
+      to_b
+    elsif float?
+      to_f
+    elsif integer?
+      to_i
+    else
+      self
+    end
+  end
+  
+  # Does this value represent an integer?
+  def integer?
+    to_i.to_s == self
+  end
+  
+  # Does this value represent a float?
+  def float?
+    to_f.to_s == self
+  end 
+
+  def boolean?
+    to_b.to_s == self
+  end
+  
+  def to_b
+    if self == 'true'
+      true
+    elsif self == 'false'
+      false
+    end
+  end
+  
+end
